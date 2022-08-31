101e7bdb5b3364711934960313e3500ef8afe46b
Juan Andres Lopez Motta <juan.lopez@smartquick.com.co>
2022-08-31 00:34:17 -0500

    Updated security config

--- src/main/java/com/matches/controllers/MatchController.java
+++ src/main/java/com/matches/controllers/MatchController.java
@@ -2,6 +2,9 @@ package com.matches.controllers;
 
 import com.matches.entities.Match;
 import com.matches.services.MatchService;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +17,11 @@ public class MatchController {
     @PostMapping("/api/match")
     public Match saveMatch(@RequestBody Match match) throws Exception{
         return matchService.saveMatch(match);
+    }
+
+    @GetMapping("/api/match/all")
+    public List<Match> getAllMatch() {
+        return matchService.getMatchList();
     }
 
     @PutMapping("/api/match/{id}")
